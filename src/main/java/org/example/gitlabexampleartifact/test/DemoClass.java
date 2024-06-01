fa11c454ecba355c1789aa55639b06ac0c28c743
shafi <mcs.shafi@gmail.com>
2024-06-01 12:38:03 +0530

    second commit

---
+++ src/main/java/org/example/gitlabexampleartifact/test/DemoClass.java
@@ -0,0 +1,8 @@
+package org.example.gitlabexampleartifact.test;
+
+public class DemoClass {
+
+    public static void demoMethod(){
+        System.out.println("Date is "+System.currentTimeMillis());
+    }
+}
