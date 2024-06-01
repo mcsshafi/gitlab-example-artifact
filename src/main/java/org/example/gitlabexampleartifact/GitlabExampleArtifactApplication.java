b165f200d25e981475292a89bc1ecdb160a7a0f0
shafi <mcs.shafi@gmail.com>
2024-06-01 12:35:09 +0530

    first commit

---
+++ src/main/java/org/example/gitlabexampleartifact/GitlabExampleArtifactApplication.java
@@ -0,0 +1,13 @@
+package org.example.gitlabexampleartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabExampleArtifactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlabExampleArtifactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gitlabexampleartifact/GitlabExampleArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gitlabexampleartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabExampleArtifactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
